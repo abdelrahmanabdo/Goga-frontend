5512e75797a5edb81df4b62c28476529250d824f
Abdelrahman abdo <dev.abdelrahman1995@gmail.com>
2020-08-21 09:23:02 +0200

    first push

---
+++ build/precache-manifest.8c8b8b66e213990d51e60b1403da573d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14026c0ba711027957dd",
+    "url": "/static/css/main.3b320181.chunk.css"
+  },
+  {
+    "revision": "14026c0ba711027957dd",
+    "url": "/static/js/main.14026c0b.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "9bfba9396b111b27f07b",
+    "url": "/static/css/2.d8790b97.chunk.css"
+  },
+  {
+    "revision": "9bfba9396b111b27f07b",
+    "url": "/static/js/2.9bfba939.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "e7e9ca9f6b49126e741ca6c940be2383",
+    "url": "/index.html"
+  }
+];
